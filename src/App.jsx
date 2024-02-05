2b186973f15b2c4d57134d2f9cb648ef14a4d37c
TATAGIBA-02 <daianefalexandria@gmail.com>
2024-02-05 15:47:26 -0300

    test

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Contador from "./components/Contador/Contador.jsx"
+
+function App(){
+return(
+<>
+<Contador />
+<h1>Olá </h1>
+</>
+
+)
+}
+
+export default App
---
+++ src/components/Contador/Contador.jsx
@@ -0,0 +1,19 @@
+import React,{useState} from "react"
+
+function Contador(){
+
+const [numero, setNumero] = useState(0)
+
+return(
+<>
+<h1>0</h1>
+<button> 1 </button>
+<button> +</button>
+<button> - </button>
+
+</>
+
+)
+
+}
+export default Contador
